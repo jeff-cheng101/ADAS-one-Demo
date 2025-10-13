ee7daf5a074d9c28e912b27d0a38195d6bf8093d
jeff.cheng <jeff.cheng@twister5.com.tw>
2025-10-13 09:13:20 +0800

    feat:將index.js格式改為index.tsx格式

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -6,7 +6,9 @@ import reportWebVitals from './reportWebVitals';
 import AIChatWidget from './AIChatWidget';
 import AIChatPanel from './AIChatPanel';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const container = document.getElementById('root');
+if (!container) throw new Error('Root container #root not found');
+const root = ReactDOM.createRoot(container);
 root.render(
   <React.StrictMode>
     <App />
@@ -15,7 +17,6 @@ root.render(
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
